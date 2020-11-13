be7048222487b8212a256707e83137dec2a1055a
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-11-13 15:06:13 +0700

    clean code

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,181 +1,221 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { OrderEntity } = require('../models');
-const { checkAuth } = require('../middlewares');
-const { verifyToken } = require('../utils');
-const { UserEntity } = require('../models/User');
-const Order = require('../models/Order');
+const { OrderEntity } = require("../models");
+const { checkAuth } = require("../middlewares");
+const { verifyToken } = require("../utils");
+const { UserEntity } = require("../models/User");
+const Order = require("../models/Order");
 
-router.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });   
+router.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-router.post('/create', (req, res, next) => checkAuth(req, res, next, 'customer'), async(req,res) => {
-    try{
-      const orderProcessing = await OrderEntity.find({$or:[{State: "Created"},{State: "Im coming"},{State: "Im going"}]});
+router.post(
+  "/create",
+  (req, res, next) => checkAuth(req, res, next, "customer"),
+  async (req, res) => {
+    try {
+      const orderProcessing = await OrderEntity.find({
+        $or: [
+          { State: "Created" },
+          { State: "Im coming" },
+          { State: "Im going" },
+        ],
+      });
       console.log(orderProcessing);
       const array = [];
-      orderProcessing.forEach(async element => {await array.push(element.Shipper)});
+      orderProcessing.forEach(async (element) => {
+        await array.push(element.Shipper);
+      });
       console.log(array);
 
-      const freeShipperList = await UserEntity.find({$and:[{role: "shipper"},{address:{$nin: array }}]});
-      
+      const freeShipperList = await UserEntity.find({
+        $and: [{ role: "shipper" }, { address: { $nin: array } }],
+      });
+
       console.log(freeShipperList);
-      
+
       console.log(freeShipperList[0].address);
 
       var time = new Date();
       let ID = time.getTime();
       const order = await new OrderEntity({
-      ID: ID,
-      Customer: req.user.address,
-      Shipper: freeShipperList[0].address,
-      Value: req.body.Value,
-      State: "Created",
-      ReportByCustomer: "",
-      ReportByShipper: ""
-    });
-    const order_ = await OrderEntity.findOne({ID: req.body.ID});
-    if (!order_) {
-    const savedOrder = await order.save();
-    return res.status(200).json(savedOrder);
-    }
-    } catch(err) {
-    console.log(err);
-    return res.status(400).json({msg: err});
-}
-})
-
-router.get('/', async(req,res) => {
-  try {
-      const order = await OrderEntity.find();
-      return res.status(200).json(order);
-  } catch(err) {
-      return res.status(400).json({msg: err});
-  }
-})
-
-router.get('/requestIDbyShipper', async(req,res) => {
-  try{
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = await verifyToken(token);
-    const order = await OrderEntity.findOne({$and:[{Shipper: decode.address},{$or:[{State: "Created"},{State: "Im coming"},{State: "Im going"}]}]});
-    return res.status(200).json(order.ID);
-  } catch(err) {
-    return res.status(400).json({msg: err});
-  }
-})
-
-router.get('/requestIDbyCustomer', async(req,res) => {
-  try{
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = await verifyToken(token);
-    const order = await OrderEntity.find({$and:[{Customer: decode.address},{State: {$nin: ["I deliveried","Cancel"]}}]});
-    return res.status(200).json(order);
-  } catch(err) {
-    return res.status(400).json({msg: err});
-  }
-})
-
-router.get('/orderListByUser', async(req,res) => {
-  try{
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = await verifyToken(token);
-    console.log(decode)
-    let order;
-    if (decode.role == "customer") 
-    {
-    order = await OrderEntity.find({Customer: `${decode.address}`});
-    }
-    else if (decode.role == "shipper") {
-    order = await OrderEntity.find({Shipper: `${decode.address}`});
-    }
-    return res.status(200).json(order);
-  } catch(err) {
-    return res.status(400).json({msg: err});
-  }
-})
-
-router.get('/:ID', async(req,res) => {
-  try {
-      const order = await OrderEntity.findOne({ID: req.params.ID});
-      return res.status(200).json(order);
-  } catch(err) {
-      return res.status(400).json({msg: err});
-  }
-})
-
-router.put('/:ID/setstate', (req, res, next) => checkAuth(req, res, next, 'shipper'), async(req,res) => {
-  try {
-    const order_ = await OrderEntity.findOne({ID: req.params.ID})
-    if (req.user.address == order_.Shipper) {
-    const order = await OrderEntity.updateOne(
-        {ID: req.params.ID}, 
-        {$set: { 
-            State: req.body.State
-          }}
-        );
-    return res.status(200).json(req.body.State);
-    }
-    else {
-      return res.status(400).json({msg: 'User is invalid'})
-    }
-} catch(err) {
-    return res.status(400).json({msg: err});
-}
-})
-
-router.put('/:ID/cancel', (req, res, next) => checkAuth(req, res, next, 'customer'), async(req,res) => {
-  try {
-    const order_ = await OrderEntity.findOne({ID: req.params.ID})
-    if (req.user.address == order_.Customer) {
-    const order = await OrderEntity.updateOne(
-        {ID: req.params.ID}, 
-        {$set: { 
-            State: "Cancel"
-          }}
-        );
-    return res.status(200).send({massage: "Cancel sucessfully"});
-    }
-    else {
-      return res.status(400).json({msg: 'User is invalid'})
-    }
-} catch(err) {
-    return res.status(400).json({msg: err});
-}
-})
-
-router.put('/:ID/report', async(req, res) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = await verifyToken(token);
-    const order_ = await OrderEntity.findOne({ID: req.params.ID})
-    if (decode.address == order_.Customer) {
-    const order = await OrderEntity.updateOne(
-        {ID: req.params.ID}, 
-        {$set: { 
-            ReportByCustomer: req.body.Report
-          }}
-        );
-    return res.status(200).json(req.body.Report);
-    }
-    else if (decode.address == order_.Shipper) {
-      const order = await OrderEntity.updateOne(
-          {ID: req.params.ID}, 
-          {$set: { 
-              ReportByShipper: req.body.Report
-            }}
-          );
-      return res.status(200).json(req.body.Report);
+        ID: ID,
+        Customer: req.user.address,
+        Shipper: freeShipperList[0].address,
+        Value: req.body.Value,
+        State: "Created",
+        ReportByCustomer: "",
+        ReportByShipper: "",
+      });
+      const order_ = await OrderEntity.findOne({ ID: req.body.ID });
+      if (!order_) {
+        const savedOrder = await order.save();
+        return res.status(200).json(savedOrder);
       }
-    else {
-      return res.status(400).json({msg: 'User is invalid'})
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ msg: err });
     }
-} catch(err) {
-    return res.status(400).json({msg: err});
-}
-})
+  }
+);
+
+router.get("/", async (req, res) => {
+  try {
+    const order = await OrderEntity.find();
+    return res.status(200).json(order);
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
+
+router.get("/requestIDbyShipper", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = await verifyToken(token);
+    const order = await OrderEntity.findOne({
+      $and: [
+        { Shipper: decode.address },
+        {
+          $or: [
+            { State: "Created" },
+            { State: "Im coming" },
+            { State: "Im going" },
+          ],
+        },
+      ],
+    });
+    return res.status(200).json(order.ID);
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
+
+router.get("/requestIDbyCustomer", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = await verifyToken(token);
+    const order = await OrderEntity.find({
+      $and: [
+        { Customer: decode.address },
+        { State: { $nin: ["I deliveried", "Cancel"] } },
+      ],
+    });
+    return res.status(200).json(order);
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
+
+router.get("/orderListByUser", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = await verifyToken(token);
+    console.log(decode);
+    let order;
+    if (decode.role == "customer") {
+      order = await OrderEntity.find({ Customer: `${decode.address}` });
+    } else if (decode.role == "shipper") {
+      order = await OrderEntity.find({ Shipper: `${decode.address}` });
+    }
+    return res.status(200).json(order);
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
+
+router.get("/:ID", async (req, res) => {
+  try {
+    const order = await OrderEntity.findOne({ ID: req.params.ID });
+    return res.status(200).json(order);
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
+
+router.put(
+  "/:ID/setstate",
+  (req, res, next) => checkAuth(req, res, next, "shipper"),
+  async (req, res) => {
+    try {
+      const order_ = await OrderEntity.findOne({ ID: req.params.ID });
+      if (req.user.address == order_.Shipper) {
+        const order = await OrderEntity.updateOne(
+          { ID: req.params.ID },
+          {
+            $set: {
+              State: req.body.State,
+            },
+          }
+        );
+        return res.status(200).json(req.body.State);
+      } else {
+        return res.status(400).json({ msg: "User is invalid" });
+      }
+    } catch (err) {
+      return res.status(400).json({ msg: err });
+    }
+  }
+);
+
+router.put(
+  "/:ID/cancel",
+  (req, res, next) => checkAuth(req, res, next, "customer"),
+  async (req, res) => {
+    try {
+      const order_ = await OrderEntity.findOne({ ID: req.params.ID });
+      if (req.user.address == order_.Customer) {
+        const order = await OrderEntity.updateOne(
+          { ID: req.params.ID },
+          {
+            $set: {
+              State: "Cancel",
+            },
+          }
+        );
+        return res.status(200).send({ massage: "Cancel sucessfully" });
+      } else {
+        return res.status(400).json({ msg: "User is invalid" });
+      }
+    } catch (err) {
+      return res.status(400).json({ msg: err });
+    }
+  }
+);
+
+router.put("/:ID/report", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = await verifyToken(token);
+    const order_ = await OrderEntity.findOne({ ID: req.params.ID });
+    if (decode.address == order_.Customer) {
+      const order = await OrderEntity.updateOne(
+        { ID: req.params.ID },
+        {
+          $set: {
+            ReportByCustomer: req.body.Report,
+          },
+        }
+      );
+      return res.status(200).json(req.body.Report);
+    } else if (decode.address == order_.Shipper) {
+      const order = await OrderEntity.updateOne(
+        { ID: req.params.ID },
+        {
+          $set: {
+            ReportByShipper: req.body.Report,
+          },
+        }
+      );
+      return res.status(200).json(req.body.Report);
+    } else {
+      return res.status(400).json({ msg: "User is invalid" });
+    }
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,147 +1,160 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { UserEntity } = require('../models');
-const { upload } = require('../middlewares');
-const { generateToken, verifyToken, hashPassword, comparePassword } = require('../utils')
-const { checkAuth } = require('../middlewares');
-const keccak256 = require('keccak256');
+const bcrypt = require("bcrypt");
+const { UserEntity } = require("../models");
+const { upload } = require("../middlewares");
+const {
+  generateToken,
+  verifyToken,
+  hashPassword,
+  comparePassword,
+} = require("../utils");
+const { checkAuth } = require("../middlewares");
+const keccak256 = require("keccak256");
 
-router.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
+router.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 
-router.put('/setAvatar/:id', upload.single('csv'), async (req, res) => {
-    try {
-        const userUpdated = await UserEntity.updateOne(
-            { _id: req.params.id },
-            {
-                $set: {
-                    avatar: req.file
-                }
-            }
-        );
-        return res.status(200).send(req.file);
-    } catch (err) {
-        return res.send(400).json({ error: err });;
-    }
-})
+router.put("/setAvatar/:id", upload.single("csv"), async (req, res) => {
+  try {
+    const userUpdated = await UserEntity.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          avatar: req.file,
+        },
+      }
+    );
+    return res.status(200).send(req.file);
+  } catch (err) {
+    return res.send(400).json({ error: err });
+  }
+});
 
-router.get('/', async (req, res) => {
-    try {
-        const user = await UserEntity.find();
-        return res.status(200).json(user);
-    } catch (err) {
-        return res.status(400).json({ error: err });
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const user = await UserEntity.find();
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
+  try {
+    if (!req.body.password)
+      res.status(400).json({ error: "Please set your password" });
+    const hashedPassword = await hashPassword(req.body.password);
+    let address = keccak256(req.body.userName).toString("hex");
+
+    const user = new UserEntity({
+      userName: req.body.userName,
+      password: hashedPassword,
+      role: req.body.role,
+      address: address,
+    });
+    const user_ = await UserEntity.findOne({ userName: req.body.userName });
+
+    if (user_ == null) {
+      const savedUser = await user.save();
+      return res.status(200).json(savedUser);
+    } else return res.status(400).json({ msg: "User is already exist" });
+  } catch (err) {
+    return res.json({ error: err });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const user = await UserEntity.findOne({ userName: req.body.userName });
+  console.log(user);
+  if (user == null) {
+    return res.status(400).send("User is not found");
+  }
+  try {
+    if (await comparePassword(req.body.password, user.password)) {
+      const token = await generateToken(user);
+      return res
+        .status(200)
+        .json({ message: `Welcome ${user.userName}`, token: token });
+    } else {
+      return res.status(500).send("Password wrong !! Please try again");
+    }
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+
+router.post("/checkrole", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = await verifyToken(token);
+    return res.status(200).json({ role: decode.role });
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await UserEntity.find({ _id: req.params.id });
+    return res.status(200).json(user);
+  } catch (err) {
+    res.json({ error: err });
+    return res.status(404);
+  }
+});
+
+router.delete(
+  "/:id",
+  (req, res, next) => checkAuth(req, res, next, "manager"),
+  async (req, res) => {
     try {
-        if (!req.body.password) res.status(400).json({ error: 'Please set your password' });
-        const hashedPassword = await hashPassword(req.body.password)
-        let address = keccak256(req.body.userName).toString('hex');
-        
-        const user = new UserEntity({
-            userName: req.body.userName,
-            password: hashedPassword,
-            role: req.body.role,
-            address: address
-        });
-        const user_ = await UserEntity.findOne({ userName: req.body.userName });
-        
-        if (user_ == null) {
-            const savedUser = await user.save();
-            return res.status(200).json(savedUser);
+      const userRemoved = await UserEntity.remove({ _id: req.params.id });
+      return res.status(200).json({ msg: "deleted" });
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
+  }
+);
+
+router.put("/:id", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const userUpdated = await UserEntity.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          password: hashedPassword,
+        },
+      }
+    );
+    return res.status(200).json({ message: "Password has changed" });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+
+router.put(
+  "/setHistory/:address",
+  (req, res, next) => checkAuth(req, res, next, "manager"),
+  async (req, res) => {
+    try {
+      const userUpdated = await UserEntity.updateOne(
+        { address: req.params.address },
+        {
+          $push: {
+            history: req.body.history,
+          },
         }
-        else return res.status(400).json({msg: 'User is already exist'});
+      );
+      return res.status(200).json({ message: "Set history successfully" });
     } catch (err) {
-        return res.json({ error: err });
+      return res.status(400).json({ error: err });
     }
-})
-
-router.post('/login', async (req, res) => {
-    const user = await UserEntity.findOne({ userName: req.body.userName });
-    console.log(user);
-    if (user == null) {
-        return res.status(400).send('User is not found');
-    }
-    try {
-        if (await comparePassword(req.body.password, user.password)) {
-            const token = await generateToken(user);
-            return res.status(200).json({message: `Welcome ${user.userName}`, token: token})
-        }
-        else {
-            return res.status(500).send("Password wrong !! Please try again")
-        }
-    } catch (err) {
-        return res.status(400).json({ error: err });
-    }
-})
-
-router.post('/checkrole', async(req,res) => {
-    try{
-        const token = req.headers.authorization.split(' ')[1];
-        const decode = await verifyToken(token);
-        return res.status(200).json({role: decode.role})
-    }
-    catch(err){
-        return res.status(400).json({msg: err});
-    }
-})
-
-router.get('/:id', async (req, res) => {
-    try {
-        const user = await UserEntity.find({ _id: req.params.id });
-        return res.status(200).json(user);
-    } catch (err) {
-        res.json({ error: err });
-        return res.status(404);
-    }
-})
-
-router.delete('/:id', (req, res, next) => checkAuth(req, res, next, 'manager'), async (req, res) => {
-    try {
-        const userRemoved = await UserEntity.remove({ _id: req.params.id });
-        return res.status(200).json({ msg: 'deleted' });
-    } catch (err) {
-        return res.status(400).json({ error: err });
-    }
-})
-
-router.put('/:id', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const userUpdated = await UserEntity.updateOne(
-            { _id: req.params.id },
-            {
-                $set: {
-                    password: hashedPassword
-                }
-            }
-        );
-        return res.status(200).json({ message: 'Password has changed' });
-    } catch (err) {
-        return res.status(400).json({ error: err });
-    }
-})
-
-router.put('/setHistory/:address', (req, res, next) => checkAuth(req, res, next, 'manager'), async (req, res) => {
-    try {
-        const userUpdated = await UserEntity.updateOne(
-            { address: req.params.address },
-            {
-                $push: {
-                    history: req.body.history
-                }
-            }
-        );
-        return res.status(200).json({ message: 'Set history successfully' });
-    } catch (err) {
-        return res.status(400).json({ error: err });
-    }
-})
+  }
+);
 
 module.exports = router;
